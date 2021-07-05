9a4564103236bee98b55e1c44d933b9b91d003f8
Mohamed Eltohamy <51927602+ttohamy@users.noreply.github.com>
2021-07-05 16:50:12 +0200

    updates

--- src/main/java/Pages/HomePage.java
+++ src/main/java/Pages/HomePage.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.support.FindBy;
 
 public class HomePage extends PageBase {
 
-    @FindBy(id ="com.android.packageinstaller:id/permission_allow_button" )
-    WebElement permissionButton ;
     @FindBy(xpath = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.widget.TextView")
     WebElement nextButton;
 
@@ -19,10 +17,6 @@ public class HomePage extends PageBase {
         super(driver);
     }
 
-    public void allowPermission(){
-        waitForVisibilityOf(permissionButton);
-        permissionButton.click();
-    }
     public void clickNext(){
         waitForVisibilityOf(nextButton);
         nextButton.click();
